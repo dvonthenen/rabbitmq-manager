9e6b3f996afe1851c59ac476cce5ca91ce08ecaa
dvonthenen <david.vonthenen@gmail.com>
2023-01-03 13:17:42 -0800

    OSS Best Practices

---
+++ docs.go
@@ -0,0 +1,14 @@
+// Copyright 2022 Symbl.ai SDK contributors. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+/*
+Package provides reference implementation for Go to handle streaming
+audio generically from CPaaS platforms.
+
+GitHub repo: https://github.com/dvonthenen/rabbitmq-patterns
+*/
+package sdk
+
+import (
+	_ "github.com/dvonthenen/rabbitmq-patterns/pkg"
+)
---
+++ pkg/rabbit-init.go
@@ -0,0 +1,42 @@
+// Copyright 2023. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package init
+
+import (
+	"flag"
+	"strconv"
+
+	klog "k8s.io/klog/v2"
+)
+
+type LogLevel int64
+
+const (
+	LogLevelDefault   LogLevel = iota
+	LogLevelErrorOnly          = 1
+	LogLevelStandard           = 2
+	LogLevelElevated           = 3
+	LogLevelFull               = 4
+	LogLevelDebug              = 5
+	LogLevelTrace              = 6
+)
+
+type RabbitInit struct {
+	LogLevel      LogLevel
+	DebugFilePath string
+}
+
+func Init(init RabbitInit) {
+	if init.LogLevel == LogLevelDefault {
+		init.LogLevel = LogLevelStandard
+	}
+
+	klog.InitFlags(nil)
+	flag.Set("v", strconv.FormatInt(int64(init.LogLevel), 10))
+	if init.DebugFilePath != "" {
+		flag.Set("logtostderr", "false")
+		flag.Set("log_file", init.DebugFilePath)
+	}
+	flag.Parse()
+}
